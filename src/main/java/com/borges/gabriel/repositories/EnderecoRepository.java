1afe8ca9cf00930b309219db76d7eb9ba7a42e3f
gabriel borges <gobcarioka@gmail.com>
2025-08-28 22:10:11 -0300

    add repositories

---
+++ src/main/java/com/borges/gabriel/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.borges.gabriel.repositories;
+
+import com.borges.gabriel.models.EnderecoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<EnderecoEntity, Long> {
+}
---
+++ src/main/java/com/borges/gabriel/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.borges.gabriel.repositories;
+
+import com.borges.gabriel.models.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+}
