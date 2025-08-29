6bf69029f4eb1cceafc4ffe3eb71a877c5854070
gabriel borges <gobcarioka@gmail.com>
2025-08-28 21:46:53 -0300

    first commit

---
+++ src/main/java/com/borges/gabriel/GabrielApplication.java
@@ -0,0 +1,13 @@
+package com.borges.gabriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielApplication.class, args);
+	}
+
+}
