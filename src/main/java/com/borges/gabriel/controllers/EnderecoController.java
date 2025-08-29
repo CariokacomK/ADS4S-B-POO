3549f1fe0e68a96d2c177c9821e54c0f78743e1d
gabriel borges <gobcarioka@gmail.com>
2025-08-28 22:25:52 -0300

    add crud Endereco

--- src/main/java/com/borges/gabriel/controllers/EnderecoController.java
+++ src/main/java/com/borges/gabriel/controllers/EnderecoController.java
@@ -2,28 +2,32 @@ package com.borges.gabriel.controllers;
 
 import com.borges.gabriel.models.EnderecoEntity;
 import com.borges.gabriel.models.UsuarioEntity;
+import com.borges.gabriel.services.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/endereco")
 public class EnderecoController {
 
+    @Autowired
+    private EnderecoService enderecoService;
     @GetMapping("/${id}")
     public EnderecoEntity buscarEndereco(@PathVariable Long id){
-        return
+        return enderecoService.buscarEnderco(id);
     }
 
     @PostMapping
     public void criarEndereco(@RequestBody EnderecoEntity endereco){
-
+        enderecoService.criarEndereco(endereco);
     }
 
     @DeleteMapping("/${id}")
     public void deletarEndereco(@PathVariable Long id){
-
+        enderecoService.deletarEndereco(id);
     }
 
     @PutMapping("/${id}")
-    public void atualizarEndereco(@PathVariable Long id, @RequestBody EnderecoEntity endereco){
-
+    public String atualizarEndereco(@PathVariable Long id, @RequestBody EnderecoEntity endereco){
+        return enderecoService.atualizarEndereco(id, endereco);
     }
 }
--- src/main/java/com/borges/gabriel/services/EnderecoService.java
+++ src/main/java/com/borges/gabriel/services/EnderecoService.java
@@ -1,9 +1,44 @@
 package com.borges.gabriel.services;
 
+import com.borges.gabriel.models.EnderecoEntity;
+import com.borges.gabriel.models.UsuarioEntity;
+import com.borges.gabriel.repositories.EnderecoRepository;
+import com.borges.gabriel.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EnderecoService {
 
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
+    public EnderecoEntity buscarEnderco(Long id){
+        return enderecoRepository.findById(id).orElse(null);
+    }
+
+    public void criarEndereco(EnderecoEntity endereco){
+        enderecoRepository.save(endereco);
+    }
+
+    public void deletarEndereco(Long id){
+        enderecoRepository.deleteById(id);
+    }
+
+    public String atualizarEndereco(Long id, EnderecoEntity endereco){
+        var enderecoOpt = enderecoRepository.findById(id).orElse(null);
+
+        if(endereco != null){
+            enderecoOpt.setBairro(endereco.getBairro());
+            enderecoOpt.setCep(endereco.getCep());
+            enderecoOpt.setNumero(endereco.getNumero());
+            enderecoOpt.setRua(endereco.getRua());
+
+            enderecoRepository.save(enderecoOpt);
+
+            return "Endereco adicionado";
+        }else{
+            return "Endereco não encontrado";
+        }
+    }
 }
