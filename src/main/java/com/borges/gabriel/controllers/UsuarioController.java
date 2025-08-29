14b441630a0cfc1261ffbb4ee2ecadb4569a705e
gabriel borges <gobcarioka@gmail.com>
2025-08-28 22:21:19 -0300

    add crud Usuario

--- src/main/java/com/borges/gabriel/controllers/UsuarioController.java
+++ src/main/java/com/borges/gabriel/controllers/UsuarioController.java
@@ -1,28 +1,33 @@
 package com.borges.gabriel.controllers;
 
 import com.borges.gabriel.models.UsuarioEntity;
+import com.borges.gabriel.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/usuario")
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping("/${id}")
     public UsuarioEntity buscarUsuario(@PathVariable Long id){
-        return
-    }
+        return usuarioService.buscarUsuario(id);
+    };
 
     @PostMapping
     public void criarUsuario(@RequestBody UsuarioEntity usuario){
-
+        usuarioService.criarUsuario(usuario);
     }
 
     @DeleteMapping("/${id}")
     public void deletarUsuario(@PathVariable Long id){
-
+        usuarioService.deletarUsuario(id);
     }
 
     @PutMapping("/${id}")
-    public void atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioEntity usuario){
-
+    public String atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioEntity usuario){
+        return usuarioService.atualizarUsuario(id, usuario);
     }
 }
--- src/main/java/com/borges/gabriel/services/UsuarioService.java
+++ src/main/java/com/borges/gabriel/services/UsuarioService.java
@@ -1,7 +1,41 @@
 package com.borges.gabriel.services;
 
+import com.borges.gabriel.models.UsuarioEntity;
+import com.borges.gabriel.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioEntity buscarUsuario(Long id){
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public void criarUsuario(UsuarioEntity usuario){
+        usuarioRepository.save(usuario);
+    }
+
+    public void deletarUsuario(Long id){
+        usuarioRepository.deleteById(id);
+    }
+
+    public String atualizarUsuario(Long id, UsuarioEntity usuario){
+        var usuarioOpt = usuarioRepository.findById(id).orElse(null);
+
+        if(usuarioOpt != null){
+            usuarioOpt.setEndereco(usuario.getEndereco());
+            usuarioOpt.setIdade(usuario.getIdade());
+            usuarioOpt.setNome(usuario.getNome());
+
+            usuarioRepository.save(usuarioOpt);
+
+            return "Usuario adicionado";
+        }else{
+            return "Usuario não encontrado";
+        }
+    }
 }
